7fc909d58d00e9496ee52678bfeef96ef9e5ba4a
Elijah Insua <tmpvar@gmail.com>
2014-04-01 23:38:16 -0700

    basic functionality

--- gcode-stream.js
+++ gcode-stream.js
@@ -1,12 +1,60 @@
-var gcode-stream = function() {
-  
-}
+var split = require('split2');
+var through = require('through2');
+var splice = require('stream-splice');
+
+
+var gcodeExp = /\W*([a-z])\W*([0-9\.\-]*)\W*/gi;
+
+
+var stringify = function(spaces) {
+  var keys = Object.keys(this);
+  var ret = [];
+
+  for (var i=0; i<keys.length; i++) {
+    if (keys[i] === 'stringify') {
+      continue;
+    }
+
+    ret.push(keys[i].toUpperCase() + this[keys[i]]);
+  }
+
+  return ret.join(spaces ? ' ' : '');
+};
+
+function gcodeLineParser(line) {
+  var parts = line.match(gcodeExp);
+
+  var ret = {};
+  if (parts) {
+    for (var i=0; i<parts.length; i++) {
+      ret[parts[i][0].toLowerCase()] = parseFloat(parts[i].substr(1).trim());
+    }
+  }
+
+  ret.stringify = stringify;
+
+  return ret;
+};
+
+var gs = function gcodeStream(transform) {
+  return splice(
+    split(),
+    through(function(line, enc, fn) {
+      var obj = gcodeLineParser(line.toString());
+      var res = transform(obj);
+      fn(null, res);
+    })
+  );
+};
+
+
+gs.parse = gcodeLineParser;
 
 
 if (typeof module !== "undefined" && typeof module.exports == "object") {
-  module.exports = gcode-stream;
+  module.exports = gs;
 }
 
 if (typeof window !== "undefined") {
-  window.gcode-stream = window.gcode-stream || gcode-stream;
+  window.gcodeStream = window.gcodeStream || gcodeStream;
 }
--- test/test.js
+++ test/test.js
@@ -1,17 +1,48 @@
-var gcode-stream;
+
 if (typeof require !== "undefined") {
-  gcode-stream = require("../gcode-stream.js");
+  var gcodeStream = require("../gcode-stream.js");
+  var split = require('split2');
 } else {
-  gcode-stream = window.gcode-stream;
+  gcodeStream = window.gcodeStream;
 }
 
 var ok = function(a, msg) { if (!a) throw new Error(msg || "not ok"); };
 var eq = function(a, b) { if (a!==b) throw new Error(a + " !== " + b); };
 
 describe('gcode-stream', function() {
-  describe('#', function() {
-    it('', function() {
-      
+  describe('#parse', function() {
+    it('should parse basic gcode', function() {
+      var obj = gcodeStream.parse('G1 X10 Y100.1Z1.24F1000');
+
+      eq(obj.g, 1);
+      eq(obj.x, 10);
+      eq(obj.y, 100.1);
+      eq(obj.z, 1.24);
+      eq(obj.f, 1000);
+
+      eq(obj.stringify(false), 'G1X10Y100.1Z1.24F1000');
+      eq(obj.stringify(true), 'G1 X10 Y100.1 Z1.24 F1000');
+    });
+  });
+
+
+  describe('#streaming', function() {
+    it('works like through', function(t) {
+      var s = gcodeStream(function(state, cb) {
+
+        if (state.f && state.f > 1000) {
+          state.f = 1000;
+        }
+
+        return state.stringify(true) + '\n';
+      });
+
+      s.pipe(split()).once('data', function(line) {
+        eq(line, 'G1 X100 Y100 Z10 F1000');
+        t();
+      });
+
+      s.end('G1 X100 Y100 Z10 F5000');
     });
   });
 });
